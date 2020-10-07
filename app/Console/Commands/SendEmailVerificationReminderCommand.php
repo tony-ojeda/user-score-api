bccb9710794996bc2170732c1292b7414d3418a3
Tony Ojeda <tojeda96@gmail.com>
2020-10-07 14:47:52 -0500

    feature: command send email 7 days

---
+++ app/Console/Commands/SendEmailVerificationReminderCommand.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Carbon\Carbon;
+use App\Models\User;
+use Illuminate\Console\Command;
+
+class SendEmailVerificationReminderCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'command:reminder';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Envia un correo electronico a los usuarios que no han verificado su cuenta despues de haberse registraod hace una semana';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        User::query()
+            ->whereDate('created_at','=',Carbon::now()->subDays(7)->format('Y-m-d'))
+            ->whereNull('email_verfied_at')
+            ->each(function(User $user) {
+              // Equivalente a $this->notify(new VerifyEmail);
+              $user->sendEmailVerificationNotification();  
+            });
+    }
+}
